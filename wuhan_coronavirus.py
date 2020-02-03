b2df3f9d80b0df1e13784c1f775181958339780b
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2020-02-04 07:59:14 +0900

    Update wuhan_coronavirus.py

--- wuhan_coronavirus.py
+++ wuhan_coronavirus.py
@@ -10,27 +10,27 @@ def linearInversion(x,y):
     return m
 
 #Population infected with Wuhan virus from 2020 Jan 15. Data from:
-#https://graphics.reuters.com/CHINA-HEALTH-MAP/0100B59S39E/index.html    
-Population = array([42,48,65,124,204,295,445,581,845,1315,2019,2800,4579,6058,7815,9820,11948,14551])
+#https://graphics.reuters.com/CHINA-HEALTH-MAP/0100B59S39E/index.html   
+#Population = array([42,48,65,124,204,295,445,581,845,1315,2019,2800,4579,6058,7815,9820,11948,14551])    
+
+#Population infected with Wuhan virus from 2020 Jan 16. Data from:
+#https://en.wikipedia.org/wiki/2019–20_Wuhan_coronavirus_outbreak
+Population = array([45,62,121,198,291,440,571,830,1287,1975,2744,4515,5974,7711,9692,11791,14380,17205,19655])
 logPopulation = log(Population)
-Deaths = array([2,2,2,3,4,6,9,17,25,41,56,80,106,132,170,213,259,304])
-logDeaths = log(Deaths)
+
 
 days = arange(1,len(Population)+1,1)
 
 mpop = linearInversion(days,logPopulation)
-#mdeath = linearInversion(days,logDeaths)
 
 print("Day population power law factor: {:.3f}".format(exp(mpop)[0]))
-#print("Day death power law factor: {:.3f}".format(exp(mdeath)[0]))
 
 #Make future predictions
-x = arange(1,30+1,1)
+x = arange(1,len(Population)+1,1)
 ypop = x * mpop[0] + mpop[1]
-#ydeaths = x * mdeath[0] + mdeath[1]
 
-plt.plot(days,logPopulation,"ro")
-plt.plot(x,ypop,"k")
+plt.semilogy(days,Population,"ro")
+plt.semilogy(x,exp(ypop),"k")
 plt.xlabel("Days")
 plt.ylabel("log_Population")
 plt.grid("on")
