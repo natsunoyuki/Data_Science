62a4c8d590d669a956369faf0573f3a6b9c6a6c6
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2020-02-09 19:34:48 +0900

    Update wuhan_coronavirus.py

--- wuhan_coronavirus.py
+++ wuhan_coronavirus.py
@@ -1,6 +1,7 @@
 from scipy import *
 import matplotlib.pyplot as plt
 from scipy import linalg
+from sklearn.linear_model import LinearRegression
 
 def linearInversion(x,y):
     N = len(x)
@@ -15,22 +16,38 @@ def linearInversion(x,y):
 
 #Population infected with Wuhan virus from 2020 Jan 16. Data from:
 #https://en.wikipedia.org/wiki/2019–20_Wuhan_coronavirus_outbreak
-Population = array([45,62,121,198,291,440,571,830,1287,1975,2744,4515,5974,7711,9692,11791,14380,17205,19655])
+Population = array([45,62,121,198,291,440,571,830,1287,1975,2744,4515,5974,7711,9692,11791,14380,17205,20438,24324,28018,31161,34546,37198])
 logPopulation = log(Population)
-
-
 days = arange(1,len(Population)+1,1)
 
-mpop = linearInversion(days,logPopulation)
+plt.plot(days,Population,'o')
+plt.xlabel("Days")
+plt.ylabel("Population")
+plt.grid("on")
+plt.show()
 
-print("Day population power law factor: {:.3f}".format(exp(mpop)[0]))
+#the power law trend seems to hold true until day 12 (index 11)...
+max_lim = 11
+mpop = linearInversion(days[:max_lim],logPopulation[:max_lim])
+print("Power law factor: {:.3f}".format(exp(mpop)[0]))
+
+#from day 18 (index 17) onwards the increase appears to follow a linear trend...
+min_lim = 17
+model = LinearRegression()
+model.fit(days[min_lim:].reshape(-1,1),Population[min_lim:])
+mpop2=[0,0]
+mpop2[0] = model.coef_[0]
+mpop2[1] = model.intercept_
+print("Linear factor: {:.3f}".format(mpop2[0]))
 
 #Make future predictions
-x = arange(1,len(Population)+1,1)
+x = arange(1,len(Population)+10,1)
 ypop = x * mpop[0] + mpop[1]
+ypop2 = x * mpop2[0] + mpop2[1]
 
 plt.semilogy(days,Population,"ro")
 plt.semilogy(x,exp(ypop),"k")
+plt.semilogy(x[min_lim:],ypop2[min_lim:],"b")
 plt.xlabel("Days")
 plt.ylabel("log_Population")
 plt.grid("on")
