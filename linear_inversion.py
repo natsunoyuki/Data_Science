ed452cdfa552447e310fbe6ed69b5adcbc1575fc
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2020-09-04 20:08:27 +0900

    Update linear_inversion.py

--- linear_inversion.py
+++ linear_inversion.py
@@ -5,19 +5,34 @@ import numpy as np
 def least_squares(d, G):
     """
     Linear inversion using least squares to get the Penrose inverse
-    For a linear system given by: d = G m
-    m_est = Gg d
+    for over determined problems.
+    For a linear system given by: |d> = G |m>
+    |m_est> = Gg |d>
+    Minimize the error E = <e|e> where |e> = |d> - G|m>
     Gg = [G.T G]**-1 G.T
     """    
     m = np.dot(np.linalg.inv(np.dot(G.T, G)), G.T)
     m = np.dot(m, d)
     return m
 
+def min_length(d, G):
+    """
+    Linear inversion using minimum length to get the Penrose inverse
+    for under determined problems.
+    For a linear system given by: |d> = G |m>
+    |m_est> = Gg |d>
+    Minimize the function: E = <m|m> + <l|e>
+    Gg = G.T [G G.T]**-1 
+    """        
+    m = np.dot(G.T, np.linalg.inv(np.dot(G, G.T)))
+    m = np.dot(m, d)
+    return m
+
 def SVD(d, G):
     """
     Linear inversion using SVD to get the Penrose inverse
-    For a linear system given by: d = G m
-    m_est = Gg d
+    For a linear system given by: |d> = G |m>
+    |m_est> = Gg |d>
     The SVD of some matrix G is given by: G = U S V
     Take only the largest eigenvalues of S, and limit the number of columns
     of U and rows of V 
