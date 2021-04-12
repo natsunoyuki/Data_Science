516659b9ac85801c2a327602055defabe1627358
Natsume <33706936+natsunoyuki@users.noreply.github.com>
2021-04-12 19:40:50 +0900

    Update linear_inversion.py

--- linear_inversion.py
+++ linear_inversion.py
@@ -1,6 +1,6 @@
-# Functions for linear inversion (linear regression).
-from scipy.optimize import linprog
+# Functions for linear inversion (regression).
 import numpy as np
+from scipy.optimize import linprog
 
 # List of linear inversion solvers in this script:
 # 1. Linear least squares inversion/regression for the over determined problem
@@ -34,12 +34,12 @@ import numpy as np
 
 def least_squares(G, d):
     """
-    Linear inversion using least squares to get the Penrose inverse
-    for over determined problems.
-    For a linear system given by: |d> = G |m>
-    |m_est> = Gg |d>
-    Minimize the error E = <e|e> where |e> = |d> - G|m>
-    Gg = [G.T G]**-1 G.T
+    Linear inversion using least squares to get the Penrose inverse.
+    For over determined problems.
+    For a linear system given by: |d> = G |m>,
+    |m_est> = Gg |d>.
+    Minimize the error E = <e|e> where |e> = |d> - G|m>.
+    Gg = [G.T G]**-1 G.T.
     
     Inputs
     ------
@@ -59,12 +59,12 @@ def least_squares(G, d):
 
 def min_length(G, d):
     """
-    Linear inversion using minimum length to get the Penrose inverse
-    for under determined problems.
-    For a linear system given by: |d> = G |m>
+    Linear inversion using minimum length to get the Penrose inverse.
+    For under determined problems.
+    For a linear system given by: |d> = G |m>,
     |m_est> = Gg |d>
-    Minimize the function: E = <m|m> + <l|e>
-    Gg = G.T [G G.T]**-1 
+    Minimize the function: E = <m|m> + <l|e>.
+    Gg = G.T [G G.T]**-1.
     
     Inputs
     ------
@@ -84,12 +84,12 @@ def min_length(G, d):
 
 def SVD(G, d, l = 0.01):
     """
-    Linear inversion using SVD to get the Penrose inverse
-    For a linear system given by: |d> = G |m>
+    Linear inversion using SVD to get the Penrose inverse.
+    For a linear system given by: |d> = G |m>,
     |m_est> = Gg |d>
-    The SVD of some matrix G is given by: G = U S V
+    The SVD of some matrix G is given by: G = U S V.
     Take only the largest eigenvalues of S, and limit the number of columns
-    of U and rows of V 
+    of U and rows of V.
     We can then obtain the Penrose inverse using the limited V, S and U.
     
     Inputs
@@ -122,7 +122,7 @@ def l1_norm_inversion(G, d, sd = 1.0):
     Linear inversion using L1 norm error instead of mean squared error for
     over determined problems.
     The inversion problem is transformed into a linear programming problem
-    and solved using the linprog() function from scipy.optimize
+    and solved using the linprog() function from scipy.optimize.
     See Geophysical Data Analysis: Discrete Inverse Theory MATLAB Edition
     Third Edition by William Menke pages 153-157 for more details.
     
@@ -174,3 +174,61 @@ def l1_norm_inversion(G, d, sd = 1.0):
     
     mest_l1 = res['x'][:M] - res['x'][M:2*M]
     return mest_l1
+
+def linf_norm_inversion(G, d, sd = 1.0):
+    """
+    Linear inversion using L-inf norm error instead of mean squared error for
+    over determined problems.
+    The inversion problem is transformed into a linear programming problem
+    and solved using the linprog() function from scipy.optimize.
+    See Geophysical Data Analysis: Discrete Inverse Theory MATLAB Edition
+    Third Edition by William Menke pages 158-160 for more details.
+    
+    Inputs
+    ------
+    G: np.array
+        np.array of the inversion kernel. Equivalent to X in scikit-learn 
+    d: np.array
+        np.array of observations. Equivalent to y in scikit-learn
+    sd: float
+        variance of the measurement d. Set to 1 by default
+        
+    Returns
+    -------
+    mest_linf: np.array
+        np.array of the inverted model parameters
+    """
+    N, M = np.shape(G)
+    L = 2 * M + 1 + 2 * N
+    f = np.zeros(L)
+    f[2*M:2*M+1] = 1
+
+    Aeq = np.zeros([2*N, L])
+    beq = np.zeros(2*N)
+    
+    Aeq[:N, :M] = G
+    Aeq[:N, M:2*M] = -G
+    Aeq[:N, 2*M:2*M+1] = -1.0 / sd
+    Aeq[:N, 2*M+1:2*M+1+N] = np.eye(N)
+    beq[:N] = d
+    
+    Aeq[N:2*N, :M] = G
+    Aeq[N:2*N, M:2*M] = -G
+    Aeq[N:2*N, 2*M:2*M+1] = -1.0 / sd
+    Aeq[N:2*N, 2*M+1+N:2*M+1+2*N] = -np.eye(N)
+    beq[N:2*N] = d
+    
+    A = np.zeros([L+2*M, L])
+    b = np.zeros(L+2*M)
+    A[:L, :] = -np.eye(L)
+    b[:L] = np.zeros(L)
+    
+    A[L:L+2*M] = np.eye(2*M, L)
+    mls = least_squares(G, d)
+    mupperbound = 10 * np.max(np.abs(mls))
+    b[L:L+2*M] = mupperbound
+    
+    res = linprog(f, A, b, Aeq, beq)
+    
+    mest_linf = res['x'][:M] - res['x'][M:2*M]
+    return mest_linf
